8a9fc34724795f721902ef9aea5b5cf6d902c4e5
zaryab.hd <zaryab.hussain91@gmail.com>
2024-05-07 14:14:03 +0500

    feat: form Validation using Formik and Yup

--- src/screens/auth/signup/index.tsx
+++ src/screens/auth/signup/index.tsx
@@ -2,33 +2,69 @@ import React from 'react';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import styles from './styles';
+import {Formik} from 'formik';
+import {validationSchema} from '../../../shared/utils/validation';
 
 const Signup = () => {
   return (
     <KeyboardAwareScrollView>
-      <View style={{flex: 1}}>
-        <View>
-          <Text style={styles.textSignup}>Sign Up</Text>
-        </View>
-        <View>
-          <TextInput
-            style={[styles.input, {marginTop: 50}]}
-            placeholder="Name"></TextInput>
-          <TextInput style={styles.input} placeholder="Email"></TextInput>
-          <TextInput style={styles.input} placeholder="Password"></TextInput>
-          <TouchableOpacity style={styles.btn}>
-            <Text style={styles.btnText}>Sign Up</Text>
-          </TouchableOpacity>
-          <Text style={styles.textOr}>or</Text>
-          <TouchableOpacity style={styles.btnSocial}>
-            <Text style={styles.btnSocialText}>sign up with google</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{alignItems: 'center'}}>
-          <Text style={styles.textAlready}>Already have an account?</Text>
-          <Text style={styles.textLogin}>Log In</Text>
-        </View>
-      </View>
+      <Formik
+        initialValues={{name: '', email: '', password: ''}}
+        onSubmit={values => console.log('Values --- :', values)}
+        validationSchema={validationSchema}>
+        {({handleChange, handleSubmit, values, errors, touched}) => (
+          <View style={{flex: 1}}>
+            <View>
+              <Text style={styles.textSignup}>Sign Up</Text>
+            </View>
+            <View>
+              <TextInput
+                style={[styles.input, {marginTop: 50}]}
+                placeholder="Name"
+                onChangeText={handleChange('name')}
+                value={values.name}></TextInput>
+              {errors.name && touched.name && (
+                <Text style={{color: 'red', marginLeft: 20}}>
+                  {errors.name}
+                </Text>
+              )}
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                onChangeText={handleChange('email')}
+                value={values.email}></TextInput>
+              {errors.email && touched.email && (
+                <Text style={{color: 'red', marginLeft: 20}}>
+                  {errors.email}
+                </Text>
+              )}
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                onChangeText={handleChange('password')}
+                value={values.password}></TextInput>
+              {errors.password && touched.password && (
+                <Text style={{color: 'red', marginLeft: 20}}>
+                  {errors.password}
+                </Text>
+              )}
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => handleSubmit()}>
+                <Text style={styles.btnText}>Sign Up</Text>
+              </TouchableOpacity>
+              <Text style={styles.textOr}>or</Text>
+              <TouchableOpacity style={styles.btnSocial}>
+                <Text style={styles.btnSocialText}>sign up with google</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.textAlready}>Already have an account?</Text>
+              <Text style={styles.textLogin}>Log In</Text>
+            </View>
+          </View>
+        )}
+      </Formik>
     </KeyboardAwareScrollView>
   );
 };
---
+++ src/shared/utils/validation.ts
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+export const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Please enter valid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .matches(/\w*[a-z]\w*/, 'Password must have a small letter')
+    .matches(/\w*[A-Z]\w*/, 'Password must have a capital letter')
+    .matches(/\d/, 'Password must have a number')
+    .min(8, ({min}) => `Password must be at least ${min} characters`)
+    .required('Password is required'),
+});
